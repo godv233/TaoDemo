6948fa29ebcd135a18cb20928e3428258cab0d96
godv233 <zengwei233@126.com>
2020-05-05 14:36:57 +0800

    已完成使用fragment碎片做中间的内容页，使用BottomNavigationView做导航栏。

--- app/src/main/java/com/example/taobaounion/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/taobaounion/ui/activity/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.taobaounion.ui.activity;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
@@ -10,15 +9,25 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.taobaounion.R;
+import com.example.taobaounion.base.BaseFragment;
+import com.example.taobaounion.ui.fragment.HomeFragment;
 import com.example.taobaounion.ui.fragment.RecommendFragment;
+import com.example.taobaounion.ui.fragment.RedPacketFragment;
+import com.example.taobaounion.ui.fragment.SearchFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG="MainActivity";
+    private static final String TAG = "MainActivity";
     @BindView(R.id.main_navigation_bar)
     BottomNavigationView mNavigationView;
+    private HomeFragment homeFragment;
+    private RecommendFragment recommendFragment;
+    private RedPacketFragment redPacketFragment;
+    private SearchFragment searchFragment;
+    FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +36,8 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         initView();
         initListener();
+        //默认选择首页
+        switchFragment(homeFragment);
     }
 
     /**
@@ -36,25 +47,39 @@ public class MainActivity extends AppCompatActivity {
         mNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Log.e(TAG,item.getTitle().toString());
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        switchFragment(homeFragment);
+                        break;
+                    case R.id.recommend:
+                        switchFragment(recommendFragment);
+                        break;
+                    case R.id.red_packet:
+                        switchFragment(redPacketFragment);
+                        break;
+                    case R.id.search:
+                        switchFragment(searchFragment);
+                        break;
+                }
                 return true;
             }
         });
+    }
+
+    private void switchFragment(BaseFragment targetFragment) {
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.main_page_container,targetFragment);
+        transaction.commit();
     }
 
     /**
      * 初始化视图
      */
     private void initView() {
-        RecommendFragment fragment = new RecommendFragment();
-        //导航栏设置监听
-
-        //事务的方式
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.main_page_container,fragment);
-        transaction.commit();
-
-
+        //初始化fragment
+        homeFragment = new HomeFragment();
+        recommendFragment = new RecommendFragment();
+        redPacketFragment = new RedPacketFragment();
+        searchFragment = new SearchFragment();
     }
 }
